63d19ec495d6ce586f19d546ff45e73ec186d172
CELALETTIN ALTINTAS <celalettinaltnts@gmail.com>
2021-04-19 00:50:52 +0300

    customer api added

---
+++ Testing.Customer/Customer.Api.Tests/CustomerControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture.Xunit2;
+using Customer.Api.Controllers;
+using Customer.Contract;
+using Customer.Service;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Customer.Api.Tests
+{
+    public class CustomerControllerTests
+    {
+        [Theory]
+        [AutoData]
+        public void GetAll_WhenCalled_ReturnExpectedCustomers(List<CustomerDto> expectedCustomers)
+        {
+            var customerServiceMock = new Mock<ICustomerService>();
+            customerServiceMock.Setup(c => c.GetAll()).Returns(expectedCustomers);
+
+            var customerController = new CustomerController(customerServiceMock.Object);
+
+            var result = customerController.Get();
+
+            var apiOkResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            apiOkResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+            var actual = apiOkResult.Value.Should().BeAssignableTo<List<CustomerDto>>().Subject;
+
+            actual.Should().BeEquivalentTo(expectedCustomers);
+        }
+
+        [Theory]
+        [AutoData]
+        public void GetById_WhenCalled_ReturnExpectedCustomer(CustomerDto customerDto, Guid customerId)
+        {
+            var customerServiceMock = new Mock<ICustomerService>();
+            customerServiceMock.Setup(c => c.GetById(customerId)).Returns(customerDto);
+
+            var customerController = new CustomerController(customerServiceMock.Object);
+
+            var result = customerController.Get(customerId);
+
+            var apiOkResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            apiOkResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+            var actual = apiOkResult.Value.Should().BeAssignableTo<CustomerDto>().Subject;
+
+            actual.Should().BeEquivalentTo(customerDto);
+        }
+    }
+}
---
+++ Testing.Customer/Customer.Api/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Customer.Contract;
+using Customer.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public ActionResult<IList<Domain.Customer>> Get()
+        {
+            return Ok(_customerService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Domain.Customer> Get(Guid customerId)
+        {
+            return Ok(_customerService.GetById(customerId));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CustomerDto customerDto)
+        {
+            _customerService.CreateNew(customerDto);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult<CustomerDto> Put([FromBody] CustomerDto customerDto)
+        {
+            return Ok(_customerService.Update(customerDto));
+        }
+
+        [HttpGet("getByCityCode/{cityCode}")]
+        public ActionResult<IList<CustomerDto>> GetByCityCode(string cityCode)
+        {
+            return Ok(_customerService.GetByCityCode(cityCode));
+        }
+    }
+}
