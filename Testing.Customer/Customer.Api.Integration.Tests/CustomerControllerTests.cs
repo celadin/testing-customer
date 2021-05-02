9127e08172b234de2a0690cb7e18b2797d0226a0
CELALETTIN ALTINTAS <celalettinaltnts@gmail.com>
2021-05-02 23:37:45 +0300

    customer controller integration tests added

--- Testing.Customer/Customer.Api.Integration.Tests/CustomerControllerTests.cs
+++ Testing.Customer/Customer.Api.Integration.Tests/CustomerControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -41,6 +43,38 @@ namespace Customer.Api.Integration.Tests
 
             Assert.Equal(expectedResult, actualResult);
             Assert.Equal(expectedStatusCode, actualStatusCode);
+        }
+
+        [Fact]
+        public async Task Get_WhenCalled_ReturnInsertedCustomer()
+        {
+            var expectedResult = string.Empty;
+            var expectedStatusCode = HttpStatusCode.OK;
+
+            var customer = new CustomerDto
+            {
+                FullName = "Celalettin Altıntaş",
+                CityCode = "Btmn",
+                BirthDate = new DateTime(1988, 05, 07)
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(customer), Encoding.UTF8, "application/json");
+
+            var postResponse = await _client.PostAsync("/api/customer", content);
+
+            var postActualResult = await postResponse.Content.ReadAsStringAsync();
+            var postActualStatusCode = postResponse.StatusCode;
+
+            Assert.Equal(expectedResult, postActualResult);
+            Assert.Equal(expectedStatusCode, postActualStatusCode);
+
+            var getResponse = await _client.GetAsync("/api/customer");
+            var getActualResult = await getResponse.Content.ReadAsStringAsync();
+            var getResult = JsonConvert.DeserializeObject<List<CustomerDto>>(getActualResult).First();
+
+            Assert.Equal(customer.FullName, getResult.FullName);
+            Assert.Equal(customer.BirthDate, getResult.BirthDate);
+            Assert.Equal(customer.CityCode, getResult.CityCode);
         }
     }
 }
